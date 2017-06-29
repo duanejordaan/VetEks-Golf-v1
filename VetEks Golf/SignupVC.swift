caf0dae65b17d6d3e97dae47172a3adc22365741
duanejordaan <duane.jordaan@gmail.com>
2017-06-29 10:28:07 +0200

    added sinup profile image selection to fb

--- VetEks Golf/SignupVC.swift
+++ VetEks Golf/SignupVC.swift
@@ -13,11 +13,16 @@ import FirebaseDatabase
 
 
 class SignUpVC: UIViewController {
-
+    
+    var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SignUpVC.handleSelectProfileImageView))
+        profileImage.addGestureRecognizer(tapGesture)
+        profileImage.isUserInteractionEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,10 +35,22 @@ class SignUpVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
+    
+    func handleSelectProfileImageView() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        present(pickerController, animated: true, completion: nil)
+    }
+    
     
     @IBAction func dismissBtnPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
     
     
     @IBAction func signUpBtnPressed(_ sender: Any) {
@@ -42,13 +59,33 @@ class SignUpVC: UIViewController {
                     print(error!.localizedDescription)
                     return
                 }
-                let ref = Database.database().reference()
-                let usersReference = ref.child("users")
-                //   print(usersReference.description()) : https://instagramclone-fc544.firebaseio.com/users
-                let uid = user?.uid
-                let newUserReference = usersReference.child(uid!)
-                newUserReference.setValue(["username": self.nameTextField.text!, "email": self.emailTextField.text!])
-                print("description: \(newUserReference.description())")
+                //let ref = Database.database().reference()
+                //let usersReference = ref.child("users")
+                
+                //let uid = user?.uid
+                //let newUserReference = usersReference.child(uid!)
+                //newUserReference.setValue(["username": self.nameTextField.text!, "email": self.emailTextField.text!])
+                //print("description: \(newUserReference.description())")
+            
+            let uid = user?.uid
+            let storageRef = Storage.storage().reference(forURL: "gs://eksvet-869e8.appspot.com").child("profile_image").child(uid!)
+            if let profileImg = self.selectedImage, let imageData = UIImageJPEGRepresentation(profileImg, 0.1) {
+                storageRef.putData(imageData, metadata: nil, completion: { (metadata, error) in
+                    if error != nil {
+                        return
+                    }
+                    
+                    let profileImageUrl = metadata?.downloadURL()?.absoluteString
+                    let ref = Database.database().reference()
+                    let usersReference = ref.child("users")
+                    let newUserReference = usersReference.child(uid!)
+                    newUserReference.setValue(["username": self.nameTextField.text!, "email": self.emailTextField.text!, "profileImageUrl": profileImageUrl])
+                })
+            
+            
+            
+            
+            
             }
     }
     
@@ -66,3 +103,16 @@ class SignUpVC: UIViewController {
     */
 
 }
+}
+extension SignUpVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("did Finish Picking Media")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            selectedImage = image
+            profileImage.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
