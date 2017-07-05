54079b10adf38d1b6d6304d877fad6b7de3015c8
duanejordaan <duane.jordaan@gmail.com>
2017-07-05 08:41:48 +0200

    created viewcontroller folder

---
+++ VetEks Golf/AuthService.swift
@@ -0,0 +1,12 @@
+//
+//  AuthService.swift
+//  VetEks Golf
+//
+//  Created by Duane Jordaan on 2017/07/05.
+//  Copyright © 2017 Duane Jordaan. All rights reserved.
+//
+
+import Foundation
+class AuthService {
+    
+}
